90c1a1a0ff5ffe9012bb3a3be279b557520a6c8c
Ruth <88747761+AwareRuuh@users.noreply.github.com>
2022-10-22 17:53:10 +0000

    finalizado

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     Scanner sc1 = new Scanner (System.in);
-    double num1, num2, soma, subtracao, divisao, multiplicacao, resto, res, potencia;
+    double num1, num2, soma, subtracao, divisao, multiplicacao, resto, potencia;
     System.out.print("Insira um número: ");
     num1 = sc1.nextDouble();
     System.out.print("Insira mais um número: ");
@@ -15,10 +15,16 @@ class Main {
     subtracao = num1 - num2;
     System.out.println("O resultado da subtração é: " + subtracao);
 
-    multiplicacao = num1 / num2;
+    multiplicacao = num1 * num2;
+    System.out.println("O resultado da multiplicacao é: " + multiplicacao);
+
+    divisao = num1 / num2;
     System.out.println("O resultado da divisão é: " + divisao);
 
     resto = num1 % num2;
     System.out.println("O resultado do resto da divisão é: " + resto);
+
+    potencia = Math.pow(num1, num2);
+    System.out.println("O resultado da potência é: " + potencia);
 }
 }
